c52979c38cdd9ee33385cae7faacf534e98ff7d3
Christoph Reiter <reiter.christoph@gmail.com>
2022-12-14 12:05:59 +0100

    Always install with "--with-all-dependencies"
    In the current case we have the follwoing failing because it installs v2
    of a dependency and then a later bundle requires v1.
    
    See https://github.com/doctrine/orm/discussions/10297
    
    We can't really stay on top of these issues of external projects, so
    allow updating/downgrading dependencies when installing bundles.

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ set -e
 
 composer create-project dbp/relay-server-template relay-api "dev-main"
 cd relay-api
-composer require dbp/relay-base-person-bundle
-composer require dbp/relay-base-person-connector-ldap-bundle
-composer require dbp/relay-frontend-bundle
-composer require dbp/relay-greenlight-bundle
-composer require dbp/relay-greenlight-connector-campusonline-bundle
-composer require dbp/relay-checkin-bundle
-composer require dbp/relay-esign-bundle
-composer require dbp/relay-formalize-bundle
-composer require dbp/relay-sublibrary-bundle
-composer require dbp/relay-template-bundle
-composer require dbp/relay-example-bundle
+composer require --with-all-dependencies dbp/relay-base-person-bundle
+composer require --with-all-dependencies dbp/relay-base-person-connector-ldap-bundle
+composer require --with-all-dependencies dbp/relay-frontend-bundle
+composer require --with-all-dependencies dbp/relay-greenlight-bundle
+composer require --with-all-dependencies dbp/relay-greenlight-connector-campusonline-bundle
+composer require --with-all-dependencies dbp/relay-checkin-bundle
+composer require --with-all-dependencies dbp/relay-esign-bundle
+composer require --with-all-dependencies dbp/relay-formalize-bundle
+composer require --with-all-dependencies dbp/relay-sublibrary-bundle
+composer require --with-all-dependencies dbp/relay-template-bundle
+composer require --with-all-dependencies dbp/relay-example-bundle
