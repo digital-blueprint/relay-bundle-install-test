d27b16d84026f9c770baae7c3c11f69e186ac751
Bekerle, Patrizio <patrizio.bekerle@tugraz.at>
2023-05-03 10:00:26 +0200

    ci: add dbp/relay-base-organization-bundle install

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ composer create-project dbp/relay-server-template relay-api "dev-main"
 cd relay-api
 composer require --with-all-dependencies dbp/relay-base-person-bundle
 composer require --with-all-dependencies dbp/relay-base-person-connector-ldap-bundle
+composer require --with-all-dependencies dbp/relay-base-organization-bundle
 composer require --with-all-dependencies dbp/relay-frontend-bundle
 composer require --with-all-dependencies dbp/relay-greenlight-bundle
 composer require --with-all-dependencies dbp/relay-greenlight-connector-campusonline-bundle
