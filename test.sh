aed83cee344c7d7010cf680175851dd3e09fea73
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-03 15:45:46 +0200

    install the latest tag, re-install auth, non-interactive by default

--- test.sh
+++ test.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-composer create-project dbp/relay-server-template relay-api "dev-main"
+composer create-project dbp/relay-server-template relay-api
 cd relay-api
-composer require --with-all-dependencies dbp/relay-base-person-bundle
-composer require --with-all-dependencies dbp/relay-base-person-connector-ldap-bundle
-composer require --with-all-dependencies dbp/relay-base-organization-bundle
-composer require --with-all-dependencies dbp/relay-frontend-bundle
-composer require --with-all-dependencies dbp/relay-greenlight-bundle
-composer require --with-all-dependencies dbp/relay-greenlight-connector-campusonline-bundle
-composer require --with-all-dependencies dbp/relay-checkin-bundle
-composer require --with-all-dependencies dbp/relay-esign-bundle
-composer require --with-all-dependencies dbp/relay-formalize-bundle
-composer require --with-all-dependencies dbp/relay-sublibrary-bundle
-composer require --with-all-dependencies dbp/relay-template-bundle
-composer require --with-all-dependencies dbp/relay-example-bundle
-composer require --with-all-dependencies dbp/relay-dispatch-bundle
+composer remove --no-interaction "dbp/relay-auth-bundle"
+composer require --no-interaction --with-all-dependencies dbp/relay-auth-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-base-person-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-base-person-connector-ldap-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-base-organization-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-frontend-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-greenlight-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-greenlight-connector-campusonline-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-checkin-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-esign-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-formalize-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-sublibrary-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-template-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-example-bundle
+composer require --no-interaction --with-all-dependencies dbp/relay-dispatch-bundle
