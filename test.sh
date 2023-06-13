b20fecd3c9003c29c1338cc32808b38d45965c74
Christoph Reiter <reiter.christoph@gmail.com>
2023-06-13 14:09:25 +0200

    Install all packages on packagist
    and skip those that don't work. This makes sure we test all new
    packages we publish.

--- test.sh
+++ test.sh
@@ -2,20 +2,39 @@
 
 set -e
 
+# get all non-abandoned packages for dbp/*
+json=$(curl --fail -s "https://packagist.org/packages/list.json?vendor=dbp&fields[]=abandoned")
+packageNames=$(echo "$json" | jq -r '.packages | to_entries | map(select(.value.abandoned == false)) | .[].key')
+
+rm -Rf relay-api
 composer create-project dbp/relay-server-template relay-api
 cd relay-api
 composer remove --no-interaction "dbp/relay-auth-bundle"
-composer require --no-interaction --with-all-dependencies dbp/relay-auth-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-base-person-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-base-person-connector-ldap-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-base-organization-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-frontend-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-greenlight-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-greenlight-connector-campusonline-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-checkin-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-esign-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-formalize-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-sublibrary-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-template-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-example-bundle
-composer require --no-interaction --with-all-dependencies dbp/relay-dispatch-bundle
+
+skipPackages=(
+    # FIXME: missing dependency
+    "dbp/relay-core-connector-ldap-bundle"
+    # FIXME: missing recipe
+    "dbp/relay-mono-bundle"
+    "dbp/relay-mono-connector-campusonline-bundle"
+    "dbp/relay-mono-connector-generic-bundle"
+    "dbp/relay-mono-connector-payunity-bundle"
+    # should be skipped because it's a template
+    "dbp/relay-server-template"
+)
+
+for packageName in $packageNames; do
+    # Skip non-relay packages
+    if [[ $packageName != "dbp/relay-"* ]]; then
+        echo "Skipping package: $packageName"
+        continue
+    fi
+
+    # Check if the package should be skipped
+    if [[ " ${skipPackages[@]} " =~ " ${packageName} " ]]; then
+        echo "Skipping package: $packageName"
+        continue
+    fi
+
+    composer require --no-interaction --with-all-dependencies "$packageName"
+done
