e8e1882dfc8987a301fa9f2fbe210d94d897f9bb
Christoph Reiter <reiter.christoph@gmail.com>
2024-04-10 13:37:04 +0200

    Unskip dbp/relay-mono-connector-payunity-bundle
    the recipe was fixed

--- test.sh
+++ test.sh
@@ -14,8 +14,6 @@ composer remove --no-interaction "dbp/relay-auth-bundle"
 echo "::endgroup::"
 
 skipPackages=(
-    # FIXME: missing recipe
-    "dbp/relay-mono-connector-payunity-bundle"
     # should be skipped because it's a template
     "dbp/relay-server-template"
 )
